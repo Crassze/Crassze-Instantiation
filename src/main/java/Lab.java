a38ef633c76967af9ba044a61ae519a7e0eed215
Jorge Madana Castano <jorge.macastano@gmail.com>
2023-03-06 17:26:22 +0000

    week2instantiateObject

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -18,6 +18,8 @@ public class Lab {
      * @return any object.
      */
     public Object instantiateANewObject(){
-        return null;
+        Lab myObj = new Lab(); 
+        return myObj;
+        
     }
 }
